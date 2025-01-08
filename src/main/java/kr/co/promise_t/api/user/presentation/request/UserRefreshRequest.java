89738c84b64452312186b2d6c8a271a502567140
kaaang <kaaang@kakao.com>
2025-01-08 20:21:40 +0900

    :bug: 리프레쉬 토큰 요청시 생성사 추가

--- src/main/java/kr/co/promise_t/api/user/presentation/request/UserRefreshRequest.java
+++ src/main/java/kr/co/promise_t/api/user/presentation/request/UserRefreshRequest.java
@@ -1,11 +1,15 @@
 package kr.co.promise_t.api.user.presentation.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRefreshRequest {
     @NotBlank private String refreshToken;
 }
